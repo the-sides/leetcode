e7510194b7e9f26e1e7c7fb1036bdb683979841e
Jacob Sides <jsides@pyxl.com>
2021-11-21 16:08:46 -0800

    V1.1 trying in the other direction. Rewrote from scratch...

--- 3sum.js
+++ 3sum.js
@@ -3,14 +3,15 @@ var threeSum = function(numss) {
     const nums = numss.sort((a, b) => a - b);    
     const results = [];
     console.log(nums)
-    for(let s1i = 0; s1i < nums.length-1; s1i++) {
+
+	let s2i = nums.length-1;
+    for(let s1i = 0; s1i < s2i; s1i++) {
         const p1 = nums[s1i];
         
         // Bc nums is sorted, once you reach an impossible combination, move on to new p1
-        let s2i = s1i + 1;
         let c2 = 0;   
         // console.log('1', s1i)
-        while(nums[s2i] !==  undefined) {
+        while(s2i > s1i) {
             // console.log('2', s2i)
             // console.log(`p1: ${p1} p2: ${nums[s2i]} p3: ...`)
             c2 = p1 + nums[s2i]
@@ -20,7 +21,7 @@ var threeSum = function(numss) {
             
             // When starting hunt for p3, don't bother with the p1s you've already seen, since 
             // The potential combination would've been possible with p1s value in that first spot
-            let s3i = s2i + 1;
+            let s3i = s2i - 1;
             let p2 = nums[s2i]
             console.log(`p1: ${p1}   p2: ${nums[s2i]}    i3: ${s3i}`)
 
@@ -30,10 +31,10 @@ var threeSum = function(numss) {
                 if(c2 + nums[s3i] > 0)   break;
                 if(c2 + nums[s3i] === 0) results.push([p1, p2, nums[s3i]]); 
                 // If you reached here, you valid
-                s3i += 1; 
+                s3i -= 1; 
             }
                   
-            s2i += 1;
+            s2i -= 1;
         }
     }
     return [...new Set(results.map(JSON.stringify))].map(set => JSON.parse(set));
